b8cc4382038b138a4d242bed6e1e0bf737fca593
E.Raj Mohan Bharathi <learn@rajmohanbtheteacher.in>
2025-05-24 21:12:20 +0530

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -2,8 +2,10 @@
 #!/bin/bash
 echo "📦 Installing Boto3..."
 pip install boto3
-
+export AWS_DEFAULT_REGION=us-east-1
 echo "🔁 Fetching ALB metadata..."
 python3 fetch_alb_metadata.py
 
 echo "✅ Done. 'alb-metadata.json' ready and served from Apache root."
+cp alb-metadata.json /var/www/html/
+echo "Copied alb-metadata.json to apache web folder ✅ Done."
